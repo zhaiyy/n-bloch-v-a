bf0ff86a06c5943137d61f8b730b749d5b277777
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-01 14:09:23 +0800

    add my blog

--- index.js
+++ index.js
@@ -1,13 +1,3 @@
-var path = require('path');
-var express = require('express');
-var app = express();
-var indexRouter = require('./routes/index');
-var userRouter = require('./routes/users');
-
-app.set('views', path.join(__dirname,'view'));
-app.set('view engine', 'ejs');
-
-app.use('/', indexRouter);
-app.use('/users', userRouter);
-
-app.listen(3000);
+/**
+ * Created by zhaiyingying on 2016/11/30.
+ */
--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,3 @@
 /**
  * Created by zhaiyingying on 2016/11/30.
  */
-var express=require('express');
-var router=express.Router();
-
-router.get('/',function (req,res) {
-    res.send('hello,express')
-});
-
-module.exports=router;

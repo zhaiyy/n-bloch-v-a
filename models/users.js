8a38e637e8c34c48993b288e2de9b24e182b5c29
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-06 11:26:53 +0800

    signin post

--- models/users.js
+++ models/users.js
@@ -6,5 +6,10 @@ var User=require('../lib/mongo').User;
 module.exports = {
     create:function (user) {
         return User.create(user).exec();
+    },
+    getUserByName:function (name) {
+        return User.findOne({name:name})
+            .addCreatedAt()
+            .exec()
     }
 }
--- routes/signin.js
+++ routes/signin.js
@@ -1,9 +1,11 @@
 /**
  * Created by zhaiyingying on 2016/11/30.
  */
+var sha1 = require('sha1');
 var express = require('express');
 var router = express.Router();
 
+var UserModel=require('../models/users')
 var checkNotLogin = require('../middleware/check').checkNotLogin;
 
 router.get('/',checkNotLogin,function (req,res,next) {
@@ -11,7 +13,25 @@ router.get('/',checkNotLogin,function (req,res,next) {
 });
 
 router.post('/',checkNotLogin,function (req,res,next) {
-    res.send(req.flash());
+    var name=req.fields.name;
+    var password=req.fields.password;
+
+    UserModel.getUserByName(name)
+        .then(function (user) {
+            if(!user){
+                req.flash('error','用户名不存在');
+                return res.redirect('back');
+            }
+            if(sha1(password)!==user.password){
+                req.flash('error','用户名或密码错误');
+                return res.redirect('back');
+            }
+            req.flash('success','登录成功');
+            delete  user.password;
+            req.session.user=user;
+            res.redirect('/posts');
+        })
+        .catch(next)
 });
 
 

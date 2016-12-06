8e08b3bf8bbbf0d38ae940a07f6ca6b876d417d4
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-06 18:34:34 +0800

    add 404

--- routes/index.js
+++ routes/index.js
@@ -9,4 +9,9 @@ module.exports = function (app) {
     app.use('/signin',require('./signin'));
     app.use('/signout',require('./signout'));
     app.use('/posts',require('./posts'));
+    app.use(function (req,res) {
+        if(!res.headersSent){
+            res.render('404')
+        }
+    });
 }

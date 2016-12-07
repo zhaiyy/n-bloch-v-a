8ec6566c32829baeac5dfc9ab3a5c0c5162412d7
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-07 15:10:00 +0800

    posts

--- models/posts.js
+++ models/posts.js
@@ -1,41 +1,43 @@
 /**
  * Created by zhaiyingying on 2016/12/2.
+ * @file
+ * @author
  */
-var marked=require('marked');
-var Post=require('../lib/mongo').Post;
-var CommentModel=require('./comments');
+var marked = require('marked');
+var Post = require('../lib/mongo').Post;
+var CommentModel = require('./comments');
 
-Post.plugin('addCommentsCount',{
-    afterFind:function (posts) {
+Post.plugin('addCommentsCount', {
+    afterFind: function (posts) {
         return Promise.all(posts.map(function (post) {
             return CommentModel.getCommentsCount(post._id).then(function (commentsCount) {
                 post.commentsCount = commentsCount;
                 return post;
-            })
+            });
 
-        }))
+        }));
     },
-    afterFindOne:function (post) {
-        if(post){
+    afterFindOne: function (post) {
+        if (post) {
             return CommentModel.getCommentsCount(post._id).then(function (count) {
                 post.commentsCount = count;
                 return post;
-            })
+            });
         }
         return post;
     }
 });
 
 
-Post.plugin('contentToHtml',{
-    afterFind:function (posts) {
+Post.plugin('contentToHtml', {
+    afterFind: function (posts) {
         return posts.map(function (post) {
             post.content = marked(post.content);
             return post;
-        })
+        });
     },
-    afterFindOne:function (post) {
-        if(post){
+    afterFindOne: function (post) {
+        if (post) {
             post.content = marked(post.content);
         }
         return post;
@@ -43,47 +45,47 @@ Post.plugin('contentToHtml',{
 });
 
 module.exports = {
-    create:function (post) {
+    create: function (post) {
         return Post.create(post).exec();
     },
-    getPostById:function (postId) {
+    getPostById: function (postId) {
         return Post
-            .findOne({_id:postId})
-            .populate({path:'author',model:'User'})
+            .findOne({_id: postId})
+            .populate({path: 'author', model: 'User'})
             .addCreatedAt()
             .addCommentsCount()
             .contentToHtml()
             .exec();
     },
-    getPosts:function (author) {
-        var query={};
-        if(author){
+    getPosts: function (author) {
+        var query = {};
+        if (author) {
             query.author = author;
         }
         return Post
             .find(query)
-            .populate({path :'author',model:'User'})
-            .sort({_id:-1})
+            .populate({path: 'author', model: 'User'})
+            .sort({_id: -1})
             .addCreatedAt()
             .addCommentsCount()
             .contentToHtml()
             .exec();
     },
-    incPv:function (postId) {
+    incPv: function (postId) {
         return Post
-            .update({_id:postId},{$inc:{pv:1}})
+            .update({_id: postId}, {$inc: {pv: 1}})
             .exec();
     },
-    getRawPostById:function (postId) {
+    getRawPostById: function (postId) {
         return Post
-            .findOne({_id:postId})
-            .populate({path:'author',model:'User'})
+            .findOne({_id: postId})
+            .populate({path: 'author', model: 'User'})
             .exec();
     },
-    updatePostById:function (postId,author,data) {
-        return Post.update({author:author,_id:postId},{$set:data}).exec();
+    updatePostById: function (postId, author, data) {
+        return Post.update({author: author, _id: postId}, {$set: data}).exec();
     },
-    delPostById:function (postId,author) {
-        return Post.remove({author:author,_id:postId}).exec();
+    delPostById: function (postId, author) {
+        return Post.remove({author: author, _id: postId}).exec();
     }
 };

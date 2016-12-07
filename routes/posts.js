0b1ffd07b0e521d509314d7378f4edacc4b291ab
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-07 14:45:46 +0800

    postas

--- routes/posts.js
+++ routes/posts.js
@@ -1,161 +1,164 @@
 /**
  * Created by zhaiyingying on 2016/11/30.
+ * @file
+ * @author
  */
 var express = require('express');
 var router = express.Router();
 
-var PostModel = require('../models/posts')
-var CommentModel = require('../models/comments')
+var PostModel = require('../models/posts');
+var CommentModel = require('../models/comments');
 var checkLogin = require('../middleware/check').checkLogin;
 
-router.get('/',function (req,res,next) {
+router.get('/', function (req, res, next) {
     var author = req.query.author;
 
-   PostModel.getPosts(author)
-      .then(function (posts) {
-            res.render('posts',{
-                posts:posts
+    PostModel.getPosts(author)
+        .then(function (posts) {
+            res.render('posts', {
+                posts: posts
             });
         })
         .catch(next);
 
 });
 
-router.get('/create',checkLogin,function (req,res,next) {
+router.get('/create', checkLogin, function (req, res, next) {
     res.render('create');
 });
 
-router.post('/',checkLogin,function (req,res,next) {
+router.post('/', checkLogin, function (req, res, next) {
     var author = req.session.user._id;
     var title = req.fields.title;
     var content = req.fields.content;
 
-    try{
-        if(!title.length){
+    try {
+        if (!title.length) {
             throw new Error('请填写标题');
         }
-        if(!content.length){
+        if (!content.length) {
             throw  new Error('请填写内容');
         }
-    }catch (e){
-        req.flash('error',e.message);
+    }
+    catch (e) {
+        req.flash('error', e.message);
         return res.redirect('back');
     }
-    var post={
-        author:author,
-        title:title,
-        content:content,
-        pv:0
+    var post = {
+        author: author,
+        title: title,
+        content: content,
+        pv: 0
     };
     PostModel.create(post)
         .then(function (result) {
             post = result.ops[0];
-            req.flash('success','发表成功');
+            req.flash('success', '发表成功');
             res.redirect(`/posts/${post._id}`);
         })
-        .catch(next)
+        .catch(next);
 });
 
 
 
-router.get('/:postId',function (req,res,next) {
-    var postId=req.params.postId;
+router.get('/:postId', function (req, res, next) {
+    var postId = req.params.postId;
 
     Promise.all([
         PostModel.getPostById(postId),
         CommentModel.getComments(postId),
-        PostModel.incPv(postId),
+        PostModel.incPv(postId)
     ])
         .then(function (result) {
-            var post =result[0];
-            var comments =result[1];
-            if(!post){
-                throw new Error('该文章不存在')
+            var post = result[0];
+            var comments = result[1];
+            if (!post) {
+                throw new Error('该文章不存在');
             }
-            res.render('post',{
-                post:post,
-                comments:comments,
+            res.render('post', {
+                post: post,
+                comments: comments
             });
         })
-        .catch(next)
+        .catch(next);
 });
 
-router.get('/:postId/edit',checkLogin,function (req,res,next) {
-    var postId=req.params.postId;
-    var author=req.session.user._id;
+router.get('/:postId/edit', checkLogin, function (req, res, next) {
+    var postId = req.params.postId;
+    var author = req.session.user._id;
 
     PostModel.getRawPostById(postId)
         .then(function (post) {
-           if(!post){
-               throw new Error('该文章不存在');
-           }
-           if(author.toString()!==post.author._id.toString()){
-               throw new Error('权限不足');
-           }
-           res.render('edit',{
-               post:post
-           });
+            if (!post) {
+                throw new Error('该文章不存在');
+            }
+            if (author.toString() !== post.author._id.toString()) {
+                throw new Error('权限不足');
+            }
+            res.render('edit', {
+                post: post
+            });
         })
-        .catch(next)
+        .catch(next);
 
 });
 
-router.post('/:postId/edit',checkLogin,function (req,res,next) {
-    var postId=req.params.postId;
-    var author=req.session.user._id;
-    var title=req.fields.title;
-    var content=req.fields.content;
+router.post('/:postId/edit', checkLogin, function (req, res, next) {
+    var postId = req.params.postId;
+    var author = req.session.user._id;
+    var title = req.fields.title;
+    var content = req.fields.content;
 
-    PostModel.updatePostById(postId,author,{title:title,content:content})
+    PostModel.updatePostById(postId, author, {title: title, content: content})
         .then(function () {
-            req.flash('success','编辑文章成功');
-            res.redirect(`/posts/${postId}`)
+            req.flash('success', '编辑文章成功');
+            res.redirect(`/posts/${postId}`);
         })
-        .catch(next)
+        .catch(next);
 });
 
-router.get('/:postId/remove',checkLogin,function (req,res,next) {
-    var postId=req.params.postId;
-    var author=req.session.user._id;
+router.get('/:postId/remove', checkLogin, function (req, res, next) {
+    var postId = req.params.postId;
+    var author = req.session.user._id;
 
-    PostModel.delPostById(postId,author)
+    PostModel.delPostById(postId, author)
         .then(function () {
-            req.flash('success','删除文章成功');
-            res.redirect('/posts')
+            req.flash('success', '删除文章成功');
+            res.redirect('/posts');
         })
-        .catch(next)
+        .catch(next);
 });
 
-router.post('/:postId/comment',checkLogin,function (req,res,next) {
-    var postId=req.params.postId;
-    var author=req.session.user._id;
-    var content=req.fields.content;
-    var comment={
-        author:author,
-        postId:postId,
-        content:content
+router.post('/:postId/comment', checkLogin, function (req, res, next) {
+    var postId = req.params.postId;
+    var author = req.session.user._id;
+    var content = req.fields.content;
+    var comment = {
+        author: author,
+        postId: postId,
+        content: content
     };
     CommentModel.create(comment)
         .then(function () {
-            req.flash('success','留言成功');
+            req.flash('success', '留言成功');
             res.redirect('back');
 
         })
-        .catch(next)
+        .catch(next);
 
 });
 
-router.get('/:postId/comment/:commentId/remove',checkLogin,function (req,res,next) {
-    var commentId=req.params.commentId;
-    var author=req.session.user._id;
+router.get('/:postId/comment/:commentId/remove', checkLogin, function (req, res, next) {
+    var commentId = req.params.commentId;
+    var author = req.session.user._id;
 
-    CommentModel.delCommentsById(commentId,author)
+    CommentModel.delCommentsById(commentId, author)
         .then(function () {
-            req.flash('success','删除留言成功');
+            req.flash('success', '删除留言成功');
             res.redirect('back');
 
         })
-        .catch(next)
+        .catch(next);
 });
 
-module.exports=router;
+module.exports = router;

ece12d4a5e29600bb215820895771afe9cf645be
翟盈盈 <zhaiyingying@zhaiyingyingdeMacBook-Pro.local>
2016-12-07 14:03:12 +0800

    index js

--- routes/index.js
+++ routes/index.js
@@ -1,22 +1,25 @@
 /**
  * Created by zhaiyingying on 2016/11/30.
+ * @file
+ * @author
+ * @app
  */
 module.exports = function (app) {
-    app.get('/',function (req,res) {
+    app.get('/', function (req, res) {
         res.redirect('/posts');
     });
-    app.use('/signup',require('./signup'));
-    app.use('/signin',require('./signin'));
-    app.use('/signout',require('./signout'));
-    app.use('/posts',require('./posts'));
-    app.use(function (req,res) {
-        if(!res.headersSent){
-            res.render('404')
+    app.use('/signup', require('./signup'));
+    app.use('/signin', require('./signin'));
+    app.use('/signout', require('./signout'));
+    app.use('/posts', require('./posts'));
+    app.use(function (req, res) {
+        if (!res.headersSent) {
+            res.render('404');
         }
     });
-    app.use(function (err,req,res,next) {
-        res.render('error',{
-            error:err
-        })
+    app.use(function (err, req, res, next) {
+        res.render('error', {
+            error: err
+        });
     });
-}
+};
